67e84268130920443118df317f3783281ef6bac7
Julio Sansossio <juliosansossio@gmail.com>
2024-08-02 22:27:40 -0400

    print log only every 50 results

--- index.ts
+++ index.ts
@@ -2,34 +2,36 @@ import pLimit from 'p-limit';
 
 const data = require('./static/original/RESULTADOS_2024_JSON_V1.json');
 
-const limit = pLimit(100);
+const max = 50;
+
+const limit = pLimit(max);
 
 const response = await Promise.all(
   data.map(async (item: any, index: number) => {
     return limit(async () => {
-      try {
-        const fileName = item.URL.split('/').pop();
-        const filePath = `./static/actas/${fileName}`;
-  
-        const newItem = {
-          ...item,
-          URL: filePath
-        };
-  
-        if (await Bun.file(filePath).exists()) {
-          return newItem;
-        }
-  
-        const response = await fetch(item.URL);
-        const buffer = Buffer.from(await response.arrayBuffer());
-  
-        await Bun.write(filePath, buffer);
-  
+      const fileName = item.URL.split('/').pop();
+      const filePath = `./static/actas/${fileName}`;
+
+      const newItem = {
+        ...item,
+        URL: filePath
+      };
+
+      if (await Bun.file(filePath).exists()) {
         return newItem;
-      } finally {
+      }
+
+      const response = await fetch(item.URL);
+      const buffer = Buffer.from(await response.arrayBuffer());
+
+      await Bun.write(filePath, buffer);
+
+      if (index % max === 0) {
         const percentage = ((index + 1) / data.length) * 100;
         console.log(`[${index + 1}/${data.length}] ${percentage.toFixed(2)}%`);
       }
+
+      return newItem
     });
   })
 );
